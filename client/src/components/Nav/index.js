5423c62ca1d9a4bf717a806ab82327c64480dae7
afellman <red.kasid@gmail.com>
2019-10-15 21:14:01 -0400

    push

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,6 +7,9 @@ function Nav() {
       <Link className="navbar-brand" to="/">
         React Reading List
       </Link>
+      <Link className="navbar-brand" to="/asdasdasd">
+        React Reading List
+      </Link>
       <Link className="navbar-brand" to="/kasdkugasdiasgudasyjgdasuyfas">
         No Match
       </Link>
